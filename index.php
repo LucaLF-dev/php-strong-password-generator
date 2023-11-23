93438e7dade33b7354b59830e001cbd98984fb5b
LucaLF-dev <laficaraluca@gmail.com>
2023-11-23 16:13:13 +0100

    Add - function getGeneratePassword and echo to html

--- index.php
+++ index.php
@@ -1,6 +1,23 @@
 <?php 
 
+$password_lenght = $_GET['password_lenght'];
 
+var_dump($password_lenght);
+
+function getGeneratePassword($text) {
+    $set_characters = '0123456789qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM!£$%&/()?@#,.;:<>';
+    $password = '';
+
+    for($i = 0; $i < $text; $i++) {
+        $password .= $set_characters[rand (0, strlen($set_characters)-1)];
+    }
+
+    return $password;
+};
+
+$password = getGeneratePassword($password_lenght);
+
+var_dump($password);
 
 ?>
 
@@ -17,10 +34,15 @@
 <div>
     <div class="container">
         <form action="" method="GET">
-               <input type="text" name="password_lenght">
+              <input type="number" name="password_lenght" id="password_lenght">
               <input type="submit" value="invia">
         </form>
    
+    </div>
+    <div>
+        <p> 
+            La tua password generata è : <?= $password ?>
+        </p>
     </div>
 </div>
     

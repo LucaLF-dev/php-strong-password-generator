edb85fb38dcd48ba5cb3ff1ba0c7da665f0debbb
LucaLF-dev <laficaraluca@gmail.com>
2023-11-23 15:48:14 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php 
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Strong Password Genarator</title>
+</head>
+<body>
+
+<div>
+    <div class="container">
+        <form action="" method="GET">
+               <input type="text" name="password_lenght">
+              <input type="submit" value="invia">
+        </form>
+   
+    </div>
+</div>
+    
+</body>
+</html>
